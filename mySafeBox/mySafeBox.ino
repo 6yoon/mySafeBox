eac37ccd0813feda585e58380afe2d95d6113970
nayun <nayunk10@gmail.com>
2024-05-30 10:17:16 +0900

    modeling

--- mySafeBox/mySafeBox.ino
+++ mySafeBox/mySafeBox.ino
@@ -7,10 +7,6 @@
 #define CLK_PIN 9
 #define DATA_PIN 10
 #define RST_PIN 11
-#define trigPin 13
-#define echoPin 12
-
-bool isClose15 = false;
 
 // Set the LCD address to 0x27 for a 16 chars and 2 line display
 LiquidCrystal_I2C lcd(0x27, 16, 2);
@@ -25,7 +21,7 @@ long interval = 1000;
 int motor_control = A1;
 Servo servo;
 
-int incline = 1;
+int incline = 0;
 bool isIncline = false;
 
 int speakerPin = A2;
@@ -72,8 +68,6 @@ void setup(){
   rtc.writeProtect(false);
   lcd.begin();
   pinMode(incline, INPUT);
-  pinMode(trigPin, OUTPUT); 
-  pinMode(echoPin, INPUT);
 }
 
 void loop(){
@@ -82,7 +76,6 @@ void loop(){
     pre = cnt;
     checkOpen();
     checkIncline();
-    checkCome();
   }
   
   //Serial.println(pCount);
@@ -239,38 +232,12 @@ void checkOpen(){
 
 void checkIncline(){
   Serial.println(digitalRead(incline));
-  if(digitalRead(incline) == HIGH && !isIncline ){
+  if(digitalRead(incline) == LOW && !isIncline ){
     Serial.println("누군가 금고를 옮기고 있습니다");
     isIncline = true;
   }
-  else if(digitalRead(incline) == LOW && isIncline){
+  else if(digitalRead(incline) == HIGH && isIncline){
     isIncline = false;
   }
   delay(100);
-}
-
-long microsecondsToCentimeters(long microseconds)
-{
-    return microseconds / 29 / 2;
-}
-
-void checkCome(){
-	long duration, cm;
-
-	digitalWrite(trigPin, LOW);
-	delayMicroseconds(2); 
-	digitalWrite(trigPin, HIGH);
-	delayMicroseconds(10); 
-	digitalWrite(trigPin, LOW);
-	duration = pulseIn(echoPin, HIGH); 
-	
-	cm = microsecondsToCentimeters(duration);
-
-  if (cm <= 15 && !isClose15) {
-		Serial.println("누군가 금고에 접근했습니다");
-    isClose15 = true;
-  } else if (cm > 15  && isClose15) {
-    isClose15 = false;
-  }
-	delay(100);   
 }
